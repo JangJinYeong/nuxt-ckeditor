fd6aaceb1558e5a54f852df763c6acf61fb222b3
nyeong <calm1025@gmail.com>
2024-05-21 22:18:56 +0900

    Add ckeditor4@4.22.1

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>CKEditor 4 in Nuxt.js</h1>
+    <div v-if="isClient">
+      <ckeditor v-model="editorData"></ckeditor>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage',
-})
+<script>
+export default {
+  data() {
+    return {
+      editorData: '<p>Content of the editor.</p>',
+      isClient: false,
+    }
+  },
+  mounted() {
+    // 컴포넌트가 클라이언트 사이드에서만 로드되도록 설정
+    this.isClient = true
+  },
+}
 </script>
